79e8b244b87f2f309d1ea18bdc176282dfd03887
Connor <cdmc13@outlook.com>
2021-05-04 13:35:38 -0400

    added definitions for background objects

--- bird_bot.py
+++ bird_bot.py
@@ -1,10 +1,13 @@
-import pygame
-import neat
-import time
-import os
-import random
+import pygame          # transform, image
+import neat            #
+import time            #
+import os              # path
+import random          #
 
 WIN_WIDTH = 600
 WIN_HEIGHT = 800
 
-BIRD_IMGS = [pygame.transform.scale2x(pygame.image.load(os.path.join("resource", "img", "bird1.png"))),pygame.transform.scale2x(pygame.image.load(os.path.join("resource", "img", "bird2.png"))),pygame.transform.scale2x(pygame.image.load(os.path.join("resource", "img", "bird3.png")))]
+BIRD_IMGS = [pygame.transform.scale2x(pygame.image.load(os.path.join("resource", "img", "bird1.png"))), pygame.transform.scale2x(pygame.image.load(os.path.join("resource", "img", "bird2.png"))), pygame.transform.scale2x(pygame.image.load(os.path.join("resource", "img", "bird3.png")))]
+PIPE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("resource", "img", "pipe.png")))
+BASE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("resource", "img", "base.png")))
+BG_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("resource", "img", "bg.png")))
