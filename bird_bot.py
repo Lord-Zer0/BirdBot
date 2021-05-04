833e52fe20144a3b30666f80cc0427915664c1ff
Connor <cdmc13@outlook.com>
2021-05-04 14:00:57 -0400

    bird class with functions

--- bird_bot.py
+++ bird_bot.py
@@ -1,8 +1,8 @@
-import pygame          # transform, image
-import neat            #
-import time            #
-import os              # path
-import random          #
+import pygame           # transform, image
+import neat             #
+import time             #
+import os               # path
+import random           #
 
 WIN_WIDTH = 600
 WIN_HEIGHT = 800
@@ -11,3 +11,71 @@ BIRD_IMGS = [pygame.transform.scale2x(pygame.image.load(os.path.join("resource",
 PIPE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("resource", "img", "pipe.png")))
 BASE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("resource", "img", "base.png")))
 BG_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("resource", "img", "bg.png")))
+
+class Bird:
+    IMGS = BIRD_IMGS
+    MAX_ROTATION = 25               # angle of rotation
+    ROT_VEL = 20                    # velocity to rotate at
+    ANIMATION_TIME = 5              # time for each animation to display
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.tilt = 0
+        self.tick_count = 0
+        self.vel = 0
+        self.height = self.y
+        self.img_count = 0
+        self.img = self.IMGS[0]
+
+    def jump(self):
+        self.vel = -10.5            # negative velocity moves upwards
+        self.tick_count = 0         # tracks when we last jumped
+        self.height = self.y
+
+    def move(self):
+        self.tick_count += 1
+
+        d = self.vel*self.tick_count + 1.5*self.tick_count**2 # d=vt+1.5t^2 (displacement formula)
+
+        if d >= 16:
+            d = 16
+
+        if d < 0:
+            d -= 2
+
+        self.y = self.y + d
+
+        if d < 0 or self.y < self.height + 50:
+            if self.tilt < self.MAX_ROTATION:
+                self.tilt = self.MAX_ROTATION:
+        else:
+            if self.tile >= -90:
+                self.tilt -= self.ROT_VEL
+
+    def draw(self, win):
+        self.img_count += 1
+
+        if  self.img_count < self.ANIMATION_TIME:
+            self.img = self.IMGS[0]
+        elif self.img_count < self.ANIMATION_TIME*2:
+            self.img = self.IMGS[1]
+        elif self.img_count < self.ANIMATION_TIME*3:
+            self.img = self.IMGS[2]
+        elif self.img_count < self.ANIMATION_TIME*4:
+            self.img = self.IMGS[1]
+        elif self.img_count < self.ANIMATION_TIME*4 + 1:
+            self.img = self.IMGS[0]
+            self.img_count = 0
+
+        if self.tilt <= -80:
+            self.img = self.IMGS[1]
+            self.img_count = self.ANIMATION_TIME*2
+
+        rotated_image = pygame.transform.rotate(self.img, self.tilt)
+        new_rect = rotated_image.get_rect(center=self.img.get_rect(topleft = (self.x, self.y)).center)
+        win.blit(rotated_image, new_rect.topleft)
+
+    def get_mask(self):
+        return pygame.mask.from_surface(self.img)
+
